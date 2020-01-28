087956d7d8613cfbfe84ab3d8fac3ad1b4412f13
Cody Lee <codylee02@gmail.com>
2020-01-27 22:15:11 -0700

    added users route

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,10 @@ const routes = require("express").Router();
 
 const auth = require("./auth");
 
+const users = require("./users");
+
 routes.use("/auth", auth);
+
+routes.use("/users", users);
 
 module.exports = routes;
